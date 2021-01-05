abed81b4940239a1a431785c844420cfeb045275
AsafSivan1 <asafisivan@gmail.com>
2021-01-05 23:43:17 +0200

    Create words.py

---
+++ src/words.py
@@ -0,0 +1,38 @@
+classifiedDictionary = {
+    "boolean": {
+        "True": "אמת",
+        "False": "שקר"
+    },
+    "if": {
+        "if": "אם",
+        'else': "אחרת",
+        "elif": "אחאם"
+    },
+    "loops": {
+        "for": "עבור",
+        "in": "בתוך",
+        "while": "כאשר",
+        "pass": "המשך",
+        "break": "שבור"
+    },
+    "packages": {
+        "import": "יבא",
+        "from": "מ"
+    },
+    "console": {
+        "print": "הדפס"
+    },
+    "functions": {
+        "def": "בסיס"
+    }, 
+    "oop": {
+        "class": "כיתה",
+        "__init__": "__יחידה__",
+        "self": "עצמאי"
+    },
+    "types": {
+        "str": "מחרוזת",
+        "int": "מספר",
+        "bool": "תנאי"
+    }
+}
